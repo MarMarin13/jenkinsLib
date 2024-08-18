77326d59275310bec31813c49f3bd8c3b31028b7
Eric <ericmarn13@gmail.com>
2024-08-18 23:56:07 +0300

    try new

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,5 +1,6 @@
 def call(String name = 'human') {
   echo "Hello, ${name}. AAAAAAA"
+  docker.build(name)
 }
 
 def build_stage(String name, String image) {
--- vars/reusableStage.groovy
+++ vars/reusableStage.groovy
@@ -1,4 +1,6 @@
 def buildStage(String name, String image) {
+    script {
     echo "Creating image ${image} in stage ${name}"
     docker.build(image)
+    }
 }
