16ab03b903a5de4e33a3f8fa540c7ab610d0fd52
Eric <ericmarn13@gmail.com>
2024-08-18 22:46:41 +0300

    test

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -2,7 +2,6 @@ def call(String name = 'human') {
   echo "Hello, ${name}. AAAAAAA"
 }
 
-def buildStage(String name, String image) {
+def build_stage(String name, String image) {
   echo "Creating image ${image} in stage ${name}"
-  docker.build(image)
 }
